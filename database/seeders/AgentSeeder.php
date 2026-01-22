eb117e6323673f1eb047b108a68909ef0f3da23f
apheep <afifkaromi264@gmail.com>
2026-01-22 18:28:00 +0700

    agne

--- database/seeders/AgentSeeder.php
+++ database/seeders/AgentSeeder.php
@@ -12,7 +12,7 @@ class AgentSeeder extends Seeder
     public function run(): void
     {
         $agents = [
-            ['email' => 'kuotaumroh@gmail.com', 'nama_pic' => 'Kuota Umroh', 'no_hp' => '081234567901', 'nama_travel' => 'Kuota Umroh', 'affiliate_id' => 1, 'jenis_travel' => 'UMROH LEISURE', 'total_traveller' => 0, 'provinsi' => 'DKI Jakarta', 'kabupaten_kota' => 'Jakarta Pusat', 'alamat_lengkap' => 'Jl. Gatot Subroto No. 1', 'kategori_agent' => 'Host', 'is_active' => true, 'link_referal' => 'kuotaumroh', 'status' => 'active'],
+            ['email' => 'kuotaumroh@gmail.com', 'nama_pic' => 'Kuota Umroh', 'no_hp' => '081234567901', 'nama_travel' => 'Kuota Umroh', 'affiliate_id' => 1, 'jenis_travel' => 'UMROH LEISURE', 'total_traveller' => 0, 'provinsi' => 'DKI Jakarta', 'kabupaten_kota' => 'Jakarta Pusat', 'alamat_lengkap' => 'Jl. Gatot Subroto No. 2', 'kategori_agent' => 'Host', 'is_active' => true, 'link_referal' => 'kuotaumroh', 'status' => 'active'],
             ];
         DB::table('agents')->insert($agents);
     }
